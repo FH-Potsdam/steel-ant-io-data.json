b13d9279b33c18f2164f89291d04b6bcf5e27c85
fabiantheblind <icke@fabianmoronzirfas.me>
2016-04-04 11:07:27 +0200

    minor fixes for jekyll

--- builder/README.md
+++ builder/README.md
@@ -1,2 +1,11 @@
-# steel-ant-io-data.json
-A combined repo for the steel ants to add there data into.
+# my sublime babel/eslint setup
+
+    npm install -g eslint babel-eslint babel-cli
+
+install the Sublime Text Packages Babel, SublimeLinter & SublimeLinter-contrib-eslint  
+
+Add the .eslintrc to your project root or your $HOME folder.  
+
+To compile es6 to es5 run:  
+
+    babel index.js -o app.js
--- builder/index.js
+++ builder/index.js
@@ -1,39 +1,7 @@
-import * as fs from 'fs';
+import as fs from 'fs';
 
-let users = ['nikoripka',
-  'martakarta',
-  'lpaubielsa',
-  'dariaDunkelbier',
-  'Ourelius',
-  'Logetcrea',
-  'Q-rec',
-  'antsteelmule',
-  'josues',
-  'Ninotschka',
-  'swjo',
-  'vogelino',
-  'wollemannone',
-  'miduku'];
-let res = [];
-fs.readFile('./example-data.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error(`Error ${err}`);
-  } else {
-    let layout = JSON.parse(data);
-    users.forEach((name, i, arr) => {
-      let user = Object.assign({}, layout['0']);
-      user.github = name;
-      res.push(user);
-    });
+let main = () =>{
 
-    console.log(res);
-    fs.writeFile('out.json', JSON.stringify(res), 'utf8', () => {
-      if (err) {
-        console.error(`Error ${err}`);
-      } else {
-        console.log('Success Wrote file.');
-      }
-    });
-  }
-});
+};
 
+main();
